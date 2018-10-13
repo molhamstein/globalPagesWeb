001deb6014b4cea9e4a58f754f010983bde0c727
Baraa <bara2@hotmail.co.uk>
2018-10-13 21:36:58 +0300

    merge fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent} from './navigation/navigation.component';
+import { HeaderWithSearchComponent } from './header-with-search/header-with-search.component';
+import { FooterComponent } from "./footer/footer.component";
+
 import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
