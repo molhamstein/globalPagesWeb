a441d59c2993c1ed930eac45b7fb051364f1f879
YazanZebak <yazanzk16@gmail.com>
2019-12-17 17:19:23 +0200

    build dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  api: 'https://almersal.co/api/',
-  server: 'https://almersal.co',
+  api: 'http://almersal.co/api/',
+  server: 'http://almersal.co',
   userDetails: '_sxamekqdw',
   language: 'kacen_ae787'
 };
